4560b190638862e0651aa14c3bbc1d32d5e58d37
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-09-03 10:40:29 -0300

    Add Header

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,17 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     :root {
-        --background: #F0F2F5
-        --red: #E52E4D
-        --blue: #5429CC
+        --background: #F0F2F5;
+        --red: #E52E4D;
+        --blue: #5429CC;
 
-        --blue-light: #6933FF
+        --blue-light: #6933FF;
 
-        --text-title: #363F5F
-        --text-body: #969Cb3
+        --text-title: #363F5F;
+        --text-body: #969Cb3;
 
-        --background: #F0F2F5
-        --shape: #FFFFFF
+        --background: #F0F2F5;
+        --shape: #FFFFFF;
     }
     
     * {
